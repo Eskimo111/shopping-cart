15178443bfd13a4156afb6dc37e6086e24aaf346
Eskimo111 <trungnguyengl176@gmail.com>
2022-10-06 20:54:41 +0700

    Change to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
@@ -13,9 +13,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <NavBar />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );

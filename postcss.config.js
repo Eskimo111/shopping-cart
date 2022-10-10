8b72d3df3f5ef49628de5ecd1b4ff6273185ba9f
Eskimo111 <trungnguyengl176@gmail.com>
2022-10-10 21:00:10 +0700

    post css

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,6 @@
 module.exports = {
-  rules: [
-    {
-      test: /\.less$/,
-      use: [
-        {
-          loader: "style-loader",
-        },
-        {
-          loader: "css-loader", // translates CSS into CommonJS
-        },
-        {
-          loader: "less-loader", // compiles Less to CSS
-        },
-      ],
-    },
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
